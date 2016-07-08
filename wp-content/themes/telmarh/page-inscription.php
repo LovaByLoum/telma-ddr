881a3e4dcbf7994c4ae2418825df92180335047c
Narisoa Andriambololona <narisoa.andriambololona@netapsys.fr>
2016-07-08 15:50:25 +0300

    ajout champ

--- wp-content/themes/telmarh/page-inscription.php
+++ wp-content/themes/telmarh/page-inscription.php
@@ -128,85 +128,87 @@ get_header(); ?>
                                                         </p>
                                                     </div>
 					                            </div>
-					                            <div class="col-1-3 form-field">
-						                            <h5>En poste<span class="required">*</span></h5>
-                                                    <label class="control control--radio">Oui
-                                                        <input type="radio"  value="0" name="en_poste" checked="checked">
-                                                        <div class="control__indicator"></div>
-                                                    </label>
-                                                    <label class="control control--radio">Non
-                                                        <input type="radio"  value="1" name="en_poste">
-                                                        <div class="control__indicator"></div>
-                                                    </label>
-					                            </div>
-					                            <p class="col-1-3 form-field">
-                                                    <input type="text" name="entreprise_user" placeholder="Nom de l'entreprise *"/>
-                                                </p>
-					                            <p class="col-1-3 form-field">
-                                                    <input type="text" name="fonction_user" placeholder="Fonction *"/>
-                                                </p>
-					                            <div class="col-1-3 form-field">
-                                                    <h5>Domaine de métier recherché</h5>
-                                                    <p class="select">
-                                                        <select>
-                                                            <option value="">Sélectionnez</option>
-                                                            <?php if ( !empty( $domainesMetier ) && count( $domainesMetier ) > 0 ):?>
-                                                                <?php foreach ( $domainesMetier as $term ):?>
-                                                                    <option value="<?php echo $term->term_id;?>"><?php echo $term->name;?></option>
-                                                                <?php endforeach;?>
-                                                            <?php endif;?>
-                                                        </select>
-                                                        <div class="select__arrow"></div>
+					                            <div class="col-1-1">
+						                            <div class="col-1-3 form-field">
+                                                        <h5>En poste<span class="required">*</span></h5>
+                                                        <label class="control control--radio">Oui
+                                                            <input type="radio"  value="0" name="en_poste" checked="checked">
+                                                            <div class="control__indicator"></div>
+                                                        </label>
+                                                        <label class="control control--radio">Non
+                                                            <input type="radio"  value="1" name="en_poste">
+                                                            <div class="control__indicator"></div>
+                                                        </label>
+                                                    </div>
+                                                    <p class="col-1-3 form-field">
+                                                        <input type="text" name="entreprise_user" placeholder="Nom de l'entreprise *"/>
                                                     </p>
-                                                </div>
-					                            <div class="col-1-3 form-field">
+                                                    <p class="col-1-3 form-field">
+                                                        <input type="text" name="fonction_user" placeholder="Fonction *"/>
+                                                    </p>
 					                            </div>
-					                            <div class="col-1-3 form-field">
-					                            </div>
-					                            <div class="col-1-3 form-field">
-                                                    <h5>Permis de conduire<span class="required">*</span></h5>
-                                                    <label class="control control--radio">Oui
-                                                        <input type="radio"  value="0" name="permis" checked="checked">
-                                                        <div class="control__indicator"></div>
-                                                    </label>
-                                                    <label class="control control--radio">Non
-                                                        <input type="radio"  value="1" name="permis">
-                                                        <div class="control__indicator"></div>
-                                                    </label>
-                                                </div>
-					                            <div class="col-1-3 form-field">
-                                                    <h5>Catégories permis<span class="required">*</span></h5>
-                                                    <label class="control control--checkbox">Permis A
-                                                        <input type="checkbox"  value="a" name="permis_cat">
-                                                        <div class="control__indicator"></div>
-                                                    </label>
-                                                    <label class="control control--checkbox">Permis A'
-                                                        <input type="checkbox"  value="ap" name="permis_cat">
-                                                        <div class="control__indicator"></div>
-                                                    </label>
-                                                    <label class="control control--checkbox">Permis B
-                                                        <input type="checkbox"  value="b" name="permis_cat">
-                                                        <div class="control__indicator"></div>
-                                                    </label>
-                                                    <label class="control control--checkbox">Permis C
-                                                        <input type="checkbox"  value="c" name="permis_cat">
-                                                        <div class="control__indicator"></div>
-                                                    </label>
-                                                </div>
-					                            <div class="col-1-3 form-field suite">
-						                            <label class="control control--checkbox">Permis D
-                                                        <input type="checkbox"  value="d" name="permis_cat">
-                                                        <div class="control__indicator"></div>
-                                                    </label>
-                                                    <label class="control control--checkbox">Permis E
-                                                        <input type="checkbox"  value="e" name="permis_cat">
-                                                        <div class="control__indicator"></div>
-                                                    </label>
-                                                    <label class="control control--checkbox">Permis F
-                                                        <input type="checkbox"  value="f" name="permis_cat">
-                                                        <div class="control__indicator"></div>
-                                                    </label>
-					                            </div>
+												<div class="col-1-1">
+													<div class="col-1-3 form-field">
+		                                                <h5>Domaine de métier recherché</h5>
+		                                                <p class="select">
+		                                                    <select>
+		                                                        <option value="">Sélectionnez</option>
+		                                                        <?php if ( !empty( $domainesMetier ) && count( $domainesMetier ) > 0 ):?>
+		                                                            <?php foreach ( $domainesMetier as $term ):?>
+		                                                                <option value="<?php echo $term->term_id;?>"><?php echo $term->name;?></option>
+		                                                            <?php endforeach;?>
+		                                                        <?php endif;?>
+		                                                    </select>
+		                                                    <div class="select__arrow"></div>
+		                                                </p>
+		                                            </div>
+												</div>
+												<div class="col-1-1">
+													<div class="col-1-3 form-field">
+	                                                    <h5>Permis de conduire<span class="required">*</span></h5>
+	                                                    <label class="control control--radio">Oui
+	                                                        <input type="radio"  value="0" name="permis" checked="checked">
+	                                                        <div class="control__indicator"></div>
+	                                                    </label>
+	                                                    <label class="control control--radio">Non
+	                                                        <input type="radio"  value="1" name="permis">
+	                                                        <div class="control__indicator"></div>
+	                                                    </label>
+	                                                </div>
+						                            <div class="col-1-3 form-field">
+	                                                    <h5>Catégories permis<span class="required">*</span></h5>
+	                                                    <label class="control control--checkbox">Permis A
+	                                                        <input type="checkbox"  value="a" name="permis_cat">
+	                                                        <div class="control__indicator"></div>
+	                                                    </label>
+	                                                    <label class="control control--checkbox">Permis A'
+	                                                        <input type="checkbox"  value="ap" name="permis_cat">
+	                                                        <div class="control__indicator"></div>
+	                                                    </label>
+	                                                    <label class="control control--checkbox">Permis B
+	                                                        <input type="checkbox"  value="b" name="permis_cat">
+	                                                        <div class="control__indicator"></div>
+	                                                    </label>
+	                                                    <label class="control control--checkbox">Permis C
+	                                                        <input type="checkbox"  value="c" name="permis_cat">
+	                                                        <div class="control__indicator"></div>
+	                                                    </label>
+	                                                </div>
+						                            <div class="col-1-3 form-field suite">
+							                            <label class="control control--checkbox">Permis D
+	                                                        <input type="checkbox"  value="d" name="permis_cat">
+	                                                        <div class="control__indicator"></div>
+	                                                    </label>
+	                                                    <label class="control control--checkbox">Permis E
+	                                                        <input type="checkbox"  value="e" name="permis_cat">
+	                                                        <div class="control__indicator"></div>
+	                                                    </label>
+	                                                    <label class="control control--checkbox">Permis F
+	                                                        <input type="checkbox"  value="f" name="permis_cat">
+	                                                        <div class="control__indicator"></div>
+	                                                    </label>
+						                            </div>
+												</div>
 				                            </div>
 			                            </div>
 
