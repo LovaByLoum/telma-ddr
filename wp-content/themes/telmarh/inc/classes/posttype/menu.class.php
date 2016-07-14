3e583cf70c52bc67625dfdb7a451ff0efedcfaaa
Narisoa Andriambololona <narisoa.andriambololona@netapsys.fr>
2016-07-14 17:04:06 +0300

    dynamisation du style du footer et suppression des options inutiles

--- wp-content/themes/telmarh/inc/classes/posttype/menu.class.php
+++ wp-content/themes/telmarh/inc/classes/posttype/menu.class.php
@@ -30,36 +30,39 @@ class CMenu
 					$hierarchyPostId[0][] = $menu_item->object_id;
 				}
 			}
-			foreach ( $hierarchy[0] as $key => $parent ) {
-				foreach ( $menu_items as $menu_list ) {
-					if ( $parent['id'] == $menu_list->menu_item_parent ) {
-						$hierarchy[$parent['id']][0][] = array(
-							'id'      => $menu_list->ID,
-							'url'     => $menu_list->url,
-							'title'   => $menu_list->title,
-							'slug'    => $parent['slug'],
-							'post_id' => $menu_list->object_id,
-						);
-						$hierarchyPostId[$parent['post_id']][0][] = $menu_list->object_id;
-					}
-				}
-				if ( isset( $hierarchy[$parent['id']][0] ) && !empty( $hierarchy[$parent['id']][0] ) ){
-					foreach ( $hierarchy[$parent['id']][0] as $key => $elementParent ){
-						foreach ( $menu_items as $menu_list ){
-							if ( $elementParent['id'] == $menu_list->menu_item_parent ) {
-								$hierarchy[$parent['id']][$elementParent['id']][] = array(
-									'id'      => $menu_list->ID,
-									'url'     => $menu_list->url,
-									'title'   => $menu_list->title,
-									'slug'    => $parent['slug'],
-									'post_id' => $menu_list->object_id,
+			if ( isset( $hierarchy[0] ) && !empty( $hierarchy[0] ) ){
+				foreach ( $hierarchy[0] as $key => $parent ) {
+					foreach ( $menu_items as $menu_list ) {
+						if ( $parent['id'] == $menu_list->menu_item_parent ) {
+							$hierarchy[$parent['id']][0][] = array(
+								'id'      => $menu_list->ID,
+								'url'     => $menu_list->url,
+								'title'   => $menu_list->title,
+								'slug'    => $parent['slug'],
+								'post_id' => $menu_list->object_id,
 							);
-							$hierarchyPostId[$parent['post_id']][$elementParent['post_id']][] = $menu_list->object_id;
+							$hierarchyPostId[$parent['post_id']][0][] = $menu_list->object_id;
+						}
+					}
+					if ( isset( $hierarchy[$parent['id']][0] ) && !empty( $hierarchy[$parent['id']][0] ) ){
+						foreach ( $hierarchy[$parent['id']][0] as $key => $elementParent ){
+							foreach ( $menu_items as $menu_list ){
+								if ( $elementParent['id'] == $menu_list->menu_item_parent ) {
+									$hierarchy[$parent['id']][$elementParent['id']][] = array(
+										'id'      => $menu_list->ID,
+										'url'     => $menu_list->url,
+										'title'   => $menu_list->title,
+										'slug'    => $parent['slug'],
+										'post_id' => $menu_list->object_id,
+								);
+								$hierarchyPostId[$parent['post_id']][$elementParent['post_id']][] = $menu_list->object_id;
+								}
+							}
 						}
 					}
 				}
-				}
 			}
+
 		}
 		return array("data" => $hierarchy, "post_id" => $hierarchyPostId);
 	}
@@ -68,8 +71,11 @@ class CMenu
 		global $telmarh_options;
 		$html = '';
 		$html .= '<ul class="menu-footer clearfix">';
+		$menuFooterElement = self::getMenuhierarchy( SLUG_MENU_FOOTER );
+		$menuFooter = ( isset( $menuFooterElement['data'] ) && !empty( $menuFooterElement['data'] ) ) ? $menuFooterElement['data'] : array();
+		$menuCount = count($menuFooter[0]) + 2;
 		if ( !empty( $telmarh_options['facebook'] ) || !empty( $telmarh_options['google'] ) || !empty( $telmarh_options['linkedin'] ) || !empty( $telmarh_options['instagramm'] ) || !empty( $telmarh_options['twitter'] )  ){
-			$html .= '<li class="col-1-4">';
+			$html .= '<li class="col-1-' . $menuCount . '">';
 			$html .= '<a href="javascript:;" title="Suivez-nous">Suivez-nous</a>';
 			$arrayKeyRs = array( 'facebook', 'google', 'linkedin', 'instagramm', 'twitter' );
 			$html .= '<ul class="social-media-icons footer">';
@@ -83,14 +89,11 @@ class CMenu
 				}
 			}
 			$html .= '</ul>';
-			$html .= '<li>';
 		}
-		$menuFooterElement = self::getMenuhierarchy( SLUG_MENU_FOOTER );
-		$menuFooter = ( isset( $menuFooterElement['data'] ) && !empty( $menuFooterElement['data'] ) ) ? $menuFooterElement['data'] : array();
 		if ( isset( $menuFooter[0] ) && !empty( $menuFooter[0] ) && count( $menuFooter[0] ) > 0 ){
 			//parcourir le parent
 			foreach ( $menuFooter[0] as $parent ) {
-				$html .= '<li class="col-1-4">';
+				$html .= '<li class="col-1-' . $menuCount . '">';
 				$html .= '<a href="' . get_permalink( $parent['url'] ) . '" >' . $parent['title'] . '</a>' ;
 				if ( isset( $menuFooter[$parent['id']][0] ) && !empty( $menuFooter[$parent['id']][0] ) ){
 					//menu child
@@ -116,7 +119,7 @@ class CMenu
 			}
 		}
 		if ( isset( $telmarh_options['description_footer'] ) && !empty( $telmarh_options['description_footer'] ) ) {
-			$html .= '<li class="col-1-4">';
+			$html .= '<li class="col-1-' . $menuCount . '">';
 			$html .= '<a href="javascript:;" title="Suivez-nous">Suivez-nous</a>';
 			$html .= '  <div class="site-info">
   							' . apply_filters("the_content", $telmarh_options['description_footer']) . '
--- wp-content/themes/telmarh/theme-options/options.php
+++ wp-content/themes/telmarh/theme-options/options.php
@@ -9,16 +9,6 @@ global $telmarh_options_settings;
 
 $telmarh_options_settings = array(
     'Configuration du pied de page'       => array(
-        'adress_site' => array(
-          'label' => 'Adresse du site',
-          'type' => 'textarea',
-          'description' => 'Ajouter ici l\'adresse du site',
-        ),
-        'phone_site' => array(
-          'label' => 'Numéro de Téléphone du site',
-          'type' => 'text',
-          'description' => 'Ajouter ici le numero de téléphone du site',
-        ),
         'copyright' => array(
           'label' => 'Copyright',
           'type' => 'text',
