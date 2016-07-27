24b3c541d6706026862e177facfdd1b9a846976d
Narisoa Andriambololona <narisoa.andriambololona@netapsys.fr>
2016-07-27 14:43:52 +0300

    INTERFACERECRUT-44

--- wp-content/themes/telmarh/js/telmarh.scripts.js
+++ wp-content/themes/telmarh/js/telmarh.scripts.js
@@ -754,7 +754,14 @@ function addDatePickerInClass( elementClass ){
         showButtonPanel : true,
         firstDay: 1,
         showButtonPanel: true,
-        closeText: 'Fermer'
+        monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        monthNamesShort: ['Janv', 'Févr', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+        dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        dayNamesShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+        dayNamesMin: ['Di', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+        weekHeader: 'Sem.',
+        closeText: 'Fermer',
+        currentText : "Ce jour"
     });
 }
 function showPreviewImage_click(event) {
