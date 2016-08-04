dc35ea0e0551c17218479e7f35483d58dba09a55
Narisoa Andriambololona <narisoa.andriambololona@netapsys.fr>
2016-08-04 10:10:19 +0300

    correction page 404

--- wp-content/themes/telmarh/404.php
+++ wp-content/themes/telmarh/404.php
@@ -17,38 +17,14 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'telmarh' ); ?></p>
-
-					<?php get_search_form(); ?>
-
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-					<?php if ( telmarh_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php _e( 'Most Used Categories', 'telmarh' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-					<?php endif; ?>
-
-					<?php
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'telmarh' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
+					<p>
+			          <strong><?php echo __('Cette page n’est malheureusement pas disponible.','telmarh');?></strong>
+			          <?php echo __('Le lien que vous avez suivi peut être incorrect ou la page peut avoir été supprimée.','telmarh');?>
+			        </p>
 				</div><!-- .page-content -->
+				<p class="btn">
+		          <a href="<?php echo $siteurl; ?>" title="Retour" class="submit_link button--wapasha button--round-l"><?php echo __('Retour','telmarh');?></a>
+		        </p>
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
