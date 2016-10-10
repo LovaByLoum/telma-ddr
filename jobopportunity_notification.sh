8c494ff475af09b45f3d41132a91565cc5d0195a
Narisoa ANdriambololona <narisoa.andriambololona@netapsys.fr>
2016-10-10 10:36:16 +0300

    INTERFACERECRUT-156

--- jobopportunity_notification.sh
+++ jobopportunity_notification.sh
@@ -5,4 +5,7 @@ then
 elif ( hostname | grep -E '^telma\.net$' > /dev/null)
 then
     wget https://telma.net/wp-cron.php --no-check-certificate -O /dev/null
+elif ( hostname | grep -E '^tapq237lv$' > /dev/null )
+then
+    wget https://telma.net/wp-cron.php --no-check-certificate -O /dev/null
 fi
