87d227fd034fafdf8aef3f768748125783753377
Narisoa ANdriambololona <narisoa.andriambololona@netapsys.fr>
2016-11-09 16:39:04 +0300

    correction du script sh

--- jobopportunity_notification.sh
+++ jobopportunity_notification.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-if ( hostname | grep -E '^tsgt02lv$' > /dev/null)
+if ( echo $HOSTNAME | egrep -i '^tsgt02lv$' > /dev/null)
 then
     wget https://tsgt02lv/jobopportunity/wp-cron.php --no-check-certificate -O /dev/null
-elif ( hostname | grep -E '^telma\.net$' > /dev/null)
+elif ( echo $HOSTNAME | egrep -i '^telma\.net$' > /dev/null)
 then
-    wget https://telma.net/axianpreprod/wp-cron.php --no-check-certificate -O /dev/null
-elif ( hostname | grep -E '^tapq237lv$' > /dev/null )
+    wget https://$HOSTNAME/axianpreprod/wp-cron.php --no-check-certificate -O /dev/null
+elif ( echo $HOSTNAME | egrep -i '^www.telma.net$' > /dev/null )
 then
-    wget https://telma.net/axianpreprod/wp-cron.php --no-check-certificate -O /dev/null
-elif ( hostname | grep -E '^recrutement\.axian-group\.com$' > /dev/null)
+    wget https://$HOSTNAME/axianpreprod/wp-cron.php --no-check-certificate -O /dev/null
+elif ( echo $HOSTNAME | egrep -i '^tapq237lv$' > /dev/null )
 then
-    wget  https://recrutement.axian-group.com/wp-cron.php --no-check-certificate -O /dev/null
-elif ( hostname | grep -E '^TAPP242LV$' > /dev/null )
+    wget https://$HOSTNAME/axianpreprod/wp-cron.php --no-check-certificate -O /dev/null
+elif ( echo $HOSTNAME | egrep -i '^TAPP242LV$' > /dev/null )
 then
-    wget  https://recrutement.axian-group.com/wp-cron.php --no-check-certificate -O /dev/null
+    wget https://$HOSTNAME/wp-cron.php --no-check-certificate -O /dev/null
+elif ( echo $HOSTNAME | egrep -i '^recrutement\.axian-group\.com$' > /dev/null )
+then
+    wget https://$HOSTNAME/wp-cron.php --no-check-certificate -O /dev/null
 fi
