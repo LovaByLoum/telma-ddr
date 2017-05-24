9ab95563d24f8a8cd13441a74314d11b906e7feb
radimby <dimbilalaina.ramangalahy@netapsys.fr>
2017-05-24 18:12:34 +0300

    Home + responsive

--- wp-content/themes/telmarh/page-templates/accueil.php
+++ wp-content/themes/telmarh/page-templates/accueil.php
@@ -53,173 +53,105 @@ list( $imageDecouvrir ) = ( isset( $blocDecouvrirImage ) && !empty( $blocDecouvr
 $imgBlocDecouvrir = $imageDecouvrir;
 
 // image pour block contatcer
-list( $imageContacter ) = ( isset( $blocConnecterImage ) && !empty( $blocConnecterImage ) ) ? wp_get_attachment_image_src( $blocConnecterImage, "full" ) : array();
+list( $imageContacter ) = ( isset( $blocContacterImage ) && !empty( $blocContacterImage ) ) ? wp_get_attachment_image_src( $blocContacterImage, "full" ) : array();
 $imgBlocContacter = $imageContacter;
 
 get_header(); ?>
-     <section id="home-hero">
-     	<div class="telmarh-hero-bg" style="background-image: url('<?php echo $imageBackground;?>');"></div>
-        <div class="telmarh-hero-content-container">
-            <div class="telmarh-hero-content">
-                <h1 class="animate-plus animate-init" data-animations="fadeInDown" data-animation-delay="0.5s">
-					<?php echo $description;?>
-                </h1>
-				<!--block connecter-->
-				<p><i class="fa <?php echo $blocConnecterPicto; ?>"></i></p>
-				<a href="<?php print_r($blocConnecterLien); ?>">
-					<p>
-						<?php print_r($blocConnecterTitre); ?>
-						<img src="<?php print_r($imgBlocConnecter); ?>" class="" width="" height="">
-					</p>
-				</a>
+<div class="home-hero"  style="background-image: url('<?php echo $imageBackground;?>');">
+    <h1 class="animate-plus animate-init logo-home" data-animations="fadeInDown" data-animation-delay="0.1s"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-rgb.png" title="Axian - Let's grow together" /></h1>
+</div>
+<div class="container">
+    <section class="cards">
+        <div class="row">
+            <!--block connecter-->
+            <div class="col-md-6 col-lg-4 card-item ">
+                <div class="connecter animate-plus animate-init" data-animations="bounceIn" data-animation-delay="0.5s" style="background-image: url(<?php print_r($imgBlocConnecter); ?>); background-color: <?php print_r($blocConnecterCouleur); ?>;">
+                    <a href="<?php print_r($blocConnecterLien); ?>">
+                        <p><i class="fa <?php echo $blocConnecterPicto; ?>"></i></p>
 
-				<!--block Recheche-->
-				<p><i class="fa <?php echo $blocRecherchePicto; ?>"></i></p>
-				<a href="<?php print_r($blocRechercheLien); ?>">
-					<p>
-						<?php print_r($blocRechercheTitre); ?>
-						<img src="<?php print_r($imgBlocRecherche); ?>" class=" " width="" height="">
-					</p>
-				</a>
-
-				<!--block Découvrir-->
-				<p><i class="fa <?php echo $blocDecouvrirPicto; ?>"></i></p>
-				<a href="<?php print_r($blocDecouvrirLien); ?>">
-					<p>
-						<?php print_r($blocDecouvrirTitre); ?>
-						<img src="<?php print_r($imgBlocDecouvrir); ?>" class=" " width="" height="">
-					</p>
-				</a>
-
-				<!--block connecter-->
-				<p><i class="fa <?php echo $blocContacterPicto; ?>"></i></p>
-				<a href="<?php print_r($blocContacterLien); ?>">
-					<p>
-						<?php print_r($blocContacterTitre); ?>
-						<img src="<?php print_r($imgBlocContacter); ?>" class=" " width="" height="">
-					</p>
-				</a>
-			</div>
-    	</div>
-    </section>
-
-	<section id="home-content">
-        <div class="grid-home grid-home-pad">
-			<div class="col-home">
-                <div id="primary" class="content-area">
-                    <main id="main" class="site-main" role="main">
-						<article id="post-636" class="post-636 page type-page status-publish hentry">
-							<div class="home-entry-content">
-								<div id="pl-636">
-									<?php if ( !empty( $blocService ) ) :?>
-									<div class="panel-grid" id="pg-636-2">
-										<div  class="panel-row-style">
-											<div class="panel-grid-cell" id="pgc-636-2-0">
-												<section id="home-services" class="services">
-													<!--titre-->
-													<?php if ( isset( $blocService['titre'] ) ):?>
-														<div class="grid grid-pad">
-															<div class="col-1-1">
-																<h3 class="widget-title">
-																	<?php echo $blocService['titre'];?>
-																</h3>
-															</div>
-														</div>
-													<?php endif;?>
-													<!--titre-->
-													<!--elements-->
-													<?php if ( isset( $blocService['element'] ) && count( $blocService['element'] ) > 0 ):?>
-														<div class="grid grid-pad">
-															<?php foreach ( $blocService['element'] as $element ):
-																	$infosLink = ( isset($element->link ) && !empty( $element->link ) ) ? $element->link : array() ;
-																	$link = ( isset( $infosLink['link'] ) && !empty( $infosLink['link'] ) ) ? $infosLink['link'] : "";
-																	$label = ( isset( $infosLink['label'] ) && !empty( $infosLink['label'] ) ) ? $infosLink['label'] : $element->titre; ?>
-															<div class="col-1-3 tri-clear">
-																<div class="single-service">
-																	<?php if ( isset( $element->fontClass ) && !empty( $element->fontClass ) ):?>
-																		<a href="<?php echo $link;?>" title="<?php echo $label;?>">
-																			<i class="fa <?php echo $element->fontClass;?> service-icon"></i>
-																		</a>
-																	<?php endif;?>
-																	<?php if ( isset( $element->titre ) && !empty( $element->titre ) ): ?>
-																		<h3 class="service-title">
-																			<a href="<?php echo $link;?>" title="<?php echo $label;?>">
-																				<?php echo $element->titre;?>
-																			</a>
-																		</h3>
-																	<?php endif;?>
-																	<?php if ( isset( $element->description ) && !empty( $element->description ) ):?>
-																		<p>
-																			<?php echo $element->description;?>
-																		</p>
-																	<?php endif;?>
-																</div>
-															</div>
-															<?php endforeach;?>
-														</div>
-													<?php endif;?>
-													<!--elements-->
-													<?php if ( isset( $blocService['lien'] ) && !empty( $blocService['lien'] ) ):?>
-													<a href="<?php echo $blocService['lien'];?>"
-													   class="submit_link button--wapasha button--round-l">
-														<?php echo $blocService['text_bouton'];?>
-													</a>
-													<?php endif;?>
-												</section>
-											</div>
-										</div>
-									</div>
-									<?php endif;?>
-									<?php if ( !empty( $offresUrgent ) && count( $offresUrgent ) > 0 ):?>
-									<div class="panel-grid" id="pg-636-7">
-										<div class="panel-grid-cell" id="pgc-636-7-0">
-											<section id="home-news" class="home-news-area">
-
-												<div class="grid grid-pad">
-													<div class="col-1-1">
-														<h3 class="widget-title">Les dernières offres urgentes</h3>
-													</div>
-													<!-- col-1-1 -->
-												</div>
-												<!-- grid -->
-
-												<div class="grid grid-pad">
-													<?php foreach( $offresUrgent as $offre ):?>
-													<div class="col-1-3 tri-clear">
-														<h5><a href="<?php echo get_permalink( $offre->id );?>" title="<?php echo $offre->titre;?>"><strong><?php echo $offre->titre;?></strong></a></h5>
-
-														<p>
-															<?php echo wp_limite_text( $offre->desc, 200 );?>
-														</p>
-														<?php if ( isset( $offre->nameEntreprise ) && !empty( $offre->nameEntreprise ) ):?>
-															<p><strong>Entreprise : </strong><em><?php echo $offre->nameEntreprise;?></em></p>
-														<?php endif;?>
-
-														<?php if ( isset( $offre->type_contrat ) && !empty( $offre->type_contrat ) ):?>
-															<p><strong>Type de contrat  : </strong><em><?php echo $offre->type_contrat;?></em></p>
-														<?php endif;?>
-														<a href="<?php echo get_permalink( $offre->id );?>" class="submit_link button--wapasha button--round-l" title="En savoir plus">
-															En savoir plus
-														</a>
-													</div>
-													<!-- col-1-3 -->
-													<?php endforeach;?>
-												</div>
-												<!-- grid -->
-
-
-											</section>
-
-										</div>
-									</div>
-									<?php endif;?>
-								</div>
-							</div>
-						</article>
-                    </main>
+                        <p>
+                            <?php print_r($blocConnecterTitre); ?>
+                        </p>
+                    </a>
                 </div>
-			</div>
+            </div>
+            <!--block Recheche-->
+            <div class="col-md-6  col-lg-4 card-item">
+                <div class="rechercher  animate-plus"  data-animations="bounceIn" data-animation-delay="0.8s" style="background-image: url(<?php print_r($imgBlocRecherche); ?>); background-color: <?php print_r($blocRechercheCouleur); ?>;">
+                    <a href="<?php print_r($blocRechercheLien); ?>">
+                        <p><i class="fa <?php echo $blocRecherchePicto; ?>"></i></p>
+                        <p>
+                            <?php print_r($blocRechercheTitre); ?>
+                        </p>
+                    </a>
+                </div>
+            </div>
+            <!--block Découvrir-->
+            <div class="col-md-6 col-lg-4 card-item">
+                <div class="decouvrir  animate-plus"  data-animations="bounceIn" data-animation-delay="1s"  style="background-image: url(<?php print_r($imgBlocDecouvrir); ?>); background-color: <?php print_r($blocContacterCouleur); ?>;">
+                    <a href="<?php print_r($blocDecouvrirLien); ?>">
+                        <p><i class="fa <?php echo $blocDecouvrirPicto; ?>"></i></p>
+
+                        <p>
+                            <?php print_r($blocDecouvrirTitre); ?>
+                        </p>
+                    </a>
+                </div>
+            </div>
+            <!--block contacter-->
+            <div class="col-lg-8 card-item">
+                <div class="contacter  animate-plus"  data-animations="bounceIn" data-animation-delay="1.2s"  style="background-image: url(<?php print_r($imgBlocContacter); ?>); background-color: <?php print_r($blocDecouvrirCouleur); ?>;">
+                    <a href="<?php print_r($blocContacterLien); ?>">
+                    <p><i class="fa <?php echo $blocContacterPicto; ?>"></i></p>
+
+                        <p>
+                            <?php print_r($blocContacterTitre); ?>
+                        </p>
+                    </a>
+                </div>
+            </div>
         </div>
-	</section>
+    </section>
+    <section class="job-last">
+        <?php if ( !empty( $blocService ) ) { ?>
+
+        <?php }?>
+        <!-- offres urgents -->
+        <?php if ( !empty( $offresUrgent ) && count( $offresUrgent ) > 0 ){ ?>
+            <h3 class="widget-title">NOS OFFRES <i class="fa fa-search"></i></h3>
+            <?php
+            $count = 0;
+            foreach( $offresUrgent as $offre ){ ?>
+                <div class="job-item">
+                    <div class="row">
+                        <figure class="col-sm-3 <?php if ( $count % 2  != 0 ) {?> right <?php }?>">figure</figure>
+                        <div class="col-sm-9">
+                            <h5><a href="<?php echo get_permalink( $offre->id );?>" title="<?php echo $offre->titre;?>"><?php echo $offre->titre;?></a> <span class="label">urgent</span></h5>
+                            <p class="summary">
+                                <?php if ( isset( $offre->nameEntreprise ) && !empty( $offre->nameEntreprise ) ):?>
+                                    Entreprise : <?php echo $offre->nameEntreprise;?><br>
+                                <?php endif;?>
+                                <?php if ( isset( $offre->type_contrat ) && !empty( $offre->type_contrat ) ):?>
+                                    Type de contrat  : <?php echo $offre->type_contrat;?><br>
+                                <?php endif;?>
+                            </p>
+
+                            <p  class="desc">
+                                <?php echo wp_limite_text( $offre->desc, 200 );?>
+                            </p>
+
+                            <p  class="link">
+                                <a href="<?php echo get_permalink( $offre->id );?>" class="link-more" title="Voir les missions">
+                                    Voir les missions
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            <?php $count ++; }?>
+        <?php }?>
+    </section>
+</div>
 
 <?php get_footer(); ?>
