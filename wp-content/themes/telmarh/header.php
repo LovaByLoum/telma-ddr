f4f3290a053c5f6ee1ed1399fe761622f7366cd6
Johnny RAKOTOARISON <johnny.rakotoarison@gmail.com>
2017-05-26 13:44:29 +0300

    appel rte.css

--- wp-content/themes/telmarh/header.php
+++ wp-content/themes/telmarh/header.php
@@ -16,6 +16,7 @@
 <?php wp_head(); ?>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" type="text/css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/custom-styles.css" type="text/css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/rte.css" type="text/css">
 </head>
 
 <body <?php body_class(); ?>>
