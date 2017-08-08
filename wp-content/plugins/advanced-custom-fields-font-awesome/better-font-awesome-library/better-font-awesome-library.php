223a5c19041f3a98fea17d06a5cad00d66e7b043
Narisoa Andriambololona <narisoa.andriambololona@netapsys.fr>
2017-08-08 18:40:47 +0300

    Hack plugins ACF font Awesome

--- wp-content/plugins/advanced-custom-fields-font-awesome/better-font-awesome-library/better-font-awesome-library.php
+++ wp-content/plugins/advanced-custom-fields-font-awesome/better-font-awesome-library/better-font-awesome-library.php
@@ -440,7 +440,33 @@ class Better_Font_Awesome_Library {
 	 * @since  1.0.0
 	 */
 	private function setup_api_data() {
-		$this->api_data = $this->fetch_api_data( self::JSDELIVR_API_URL );
+		//$this->api_data = $this->fetch_api_data( self::JSDELIVR_API_URL );
+        $this->api_data = new stdClass();
+        $this->api_data->versions = array(
+            "4.7.0",
+            "4.6.3",
+            "4.6.2",
+            "4.6.1",
+            "4.6.0",
+            "4.5.0",
+            "4.4.0",
+            "4.3.0",
+            "4.2.0",
+            "4.1.0",
+            "4.0.3",
+            "4.0.1",
+            "4.0.0",
+            "3.2.1",
+            "3.2.0",
+            "3.1.1",
+            "3.0.2",
+            "3.0",
+            "2.0",
+            "1.0.0"
+        );
+        $this->api_data->lastversion = "4.7.0";
+
+
 	}
 
 	/**
