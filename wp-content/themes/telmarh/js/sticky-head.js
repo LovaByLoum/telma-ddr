6fbaf849ece45c98a5ef7422b506265cbb9228d0
Narisoa Andriambololona <narisoa.andriambololona@netapsys.fr>
2017-08-09 08:55:25 +0300

    correction erreur js

--- wp-content/themes/telmarh/js/sticky-head.js
+++ wp-content/themes/telmarh/js/sticky-head.js
@@ -1,15 +1,19 @@
 // Set options
-        var options = { 
-            offset: 200, 
+        if ( jQuery('.site-header').size() > 0 ){
+        var options = {
+            offset: 200,
             classes: {
                 clone:   'banner--clone',
                 stick:   'banner--stick',
-                unstick: 'banner--unstick'  
-            } 
+                unstick: 'banner--unstick'
+            }
         };
 
-        // Initialise with options
-        var banner = new Headhesive('.site-header', options);
+            // Initialise with options
+            var banner = new Headhesive('.site-header', options);
+
+        }
+
 
         // Headhesive destroy
         // banner.destroy(); 
