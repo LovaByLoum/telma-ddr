eb9a017cdedf45f5654313097420982da655a705
Judicael RAMANAMPISON <judicael.ramanampison@netapsys.fr>
2017-09-28 17:20:34 +0300

    css slider and grid

--- wp-content/themes/telmarh/page.php
+++ wp-content/themes/telmarh/page.php
@@ -47,7 +47,7 @@ $blocPartenaires = CPage::getAllpartnerHp( $frontpage_id );
     <!-- slider -->
     <?php if ( !empty($slider_partenaire[0]) ): ?>
         <?php if ( isset( $blocPartenaires ) && !empty( $blocPartenaires ) ):?>
-            <div class="panel-grid" id="pg-636-6">
+            <div class="panel-grid container" id="pg-636-6">
                 <div  class="panel-row-style">
                     <div class="panel-grid-cell" id="pgc-636-6-0">
                         <section id="home-clients" class="clients">
