1d53f6eb64c2642973c89a450acb299fa629b231
Johary <johary.ranarimanana@axian-group.com>
2019-02-14 11:27:43 +0300

    maj wp seo

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInite7d60f762f551bb51f5ab20af77eb24d::getLoader();
+return ComposerAutoloaderInitee2580dddf83278d62011ea177ca7bd4::getLoader();
