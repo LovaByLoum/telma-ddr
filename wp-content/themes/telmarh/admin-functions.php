c4c9787687866ca80975806d945f77b72fe44504
Johary <johary.ranarimanana@axian-group.com>
2019-05-22 11:56:48 +0300

    INTERFACERECRUT-241

--- wp-content/themes/telmarh/admin-functions.php
+++ wp-content/themes/telmarh/admin-functions.php
@@ -7,7 +7,7 @@
  * @author : Netapsys
  */
 
-add_action( 'admin_enqueue_scripts', 'telmarh_admin_enqueue_scripts' );
+add_action( 'admin_enqueue_scripts', 'telmarh_admin_enqueue_scripts', 999 );
 function telmarh_admin_enqueue_scripts(){
     wp_enqueue_style( 'admin-style', get_template_directory_uri() . '/css/admin/styles.css' );
 }
