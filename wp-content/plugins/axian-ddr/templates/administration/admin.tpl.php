2f7be9b554bc9bb215207f2a13b1487580b87c8a
felix <felix77@yahoo.fr>
2019-06-07 14:49:36 +0300

    ajout onglet workflow

--- wp-content/plugins/axian-ddr/templates/administration/admin.tpl.php
+++ wp-content/plugins/axian-ddr/templates/administration/admin.tpl.php
@@ -8,6 +8,7 @@
         <a href="?page=axian-ddr-admin&amp;tab=general" class="nav-tab ">Configuration générale</a>
         <a href="?page=axian-ddr-admin&amp;tab=term" class="nav-tab <?php echo $active_tab == 'term' ? 'nav-tab-active' : ''; ?>">Termes de taxonomie</a>
         <a href="?page=axian-ddr-admin&amp;tab=validation" class="nav-tab <?php echo $active_tab == 'validation' ? 'nav-tab-active' : ''; ?>">Validation</a>
+        <a href="?page=axian-ddr-admin&amp;tab=workflow" class="nav-tab <?php echo $active_tab == 'workflow' ? 'nav-tab-active' : ''; ?>">Workflow</a>
 
     </h2>
 </div>
